3669161de7b481eb18b1132c8171543e193bab7c
home\john <785562850@qq.com>
2017-10-02 00:04:31 +0800

    second commit

---
+++ src/DesignPattern/AdapterPattern/AdvancedMediaPlayer.java
@@ -0,0 +1,9 @@
+package DesignPattern.AdapterPattern;
+
+/**
+ * Created by john on 2017/10/1.
+ */
+public interface AdvancedMediaPlayer {
+    public void playMp4(String fileName);
+    public void playVlc(String fileName);
+}
